49c6971318453ffc30a14290dea65c664de2c646
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-19 14:01:51 +0200

    Uppgift 6

---
+++ 6/script.js
@@ -0,0 +1,13 @@
+
+function isSameUser(a, b){
+    return a === b
+}
+
+console.log(isSameUser("emma", "emma"))
+console.log(isSameUser("emma", "einar"))
+
+
+//hur gör man en property för ett objekt?
+
+const user1 = a
+const user2 = b
