b032f7997d09000c77b2808dd695138014bfce85
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-19 13:46:49 +0200

    Uppgift 3

---
+++ 3/script.js
@@ -0,0 +1,13 @@
+// sakpa en arrey med 5 strängar
+
+let namn = ["Fia", "Klas", "Albert", "Clara", "Aron"]
+
+// Byt sedan ut första och sista elementet mot valfria tal
+
+namn.pop(1)
+namn.push(21)
+
+namn.pop(4)
+namn.push(4)
+
+console.log(namn)
