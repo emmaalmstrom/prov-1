3a94afc06287db983dd8687126001078ced21074
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-19 13:39:09 +0200

    Uppgift 2

---
+++ 2/script.js
@@ -0,0 +1,9 @@
+// definiera en funktion sum som accepterar tre tal som argument och returnerar summan av dom
+
+function sum(a, b, c ){
+    return a + b + c
+}
+
+
+// test
+console.log(sum(1, 2, 3 ))
