fee4c219e17b822b03479066278520d2d6bbdce1
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-19 13:54:49 +0200

    Uppgift 5

--- 4/script.js
+++ 4/script.js
@@ -2,4 +2,5 @@ function lastElement(array){
     return array.lastElement
 }
 
+// test
 console.log(lastElement(["hej", "jag", "heter"]))
---
+++ 5/script.js
@@ -0,0 +1,7 @@
+function areaOfTriangel(b, h){
+    return b * h / 2
+}
+
+//test
+console.log(areaOfTriangel(2, 5))
+console.log(areaOfTriangel(6, 7))
